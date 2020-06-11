f06ecea0a02c2b861171bdd2faa47aae0c5c8c7c
Anas <anbellouzi@gmail.com>
2020-06-11 16:26:09 -0700

    crud user profile complete

--- clients/views.py
+++ clients/views.py
@@ -7,6 +7,11 @@ from dateutil import parser
 # from .models import Mood
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth.models import User
+from django.shortcuts import render, HttpResponseRedirect, get_object_or_404
+from django.urls import reverse, reverse_lazy
+from .models import Profile
+
 
 class IndexView(ListView):
     def get(self, request):
@@ -20,6 +25,27 @@ class DashboardView(ListView):
 
 class UserProfileView(ListView):
     def get(self, request):
-        context = {'data': 'self.getWeatherData()'}
-
+        user = User.objects.get(pk=request.user.id)
+        name = str(user.first_name)+" "+str(user.last_name)
+        context = {"user": user, "name": name}
+        
         return render(request, "clients/user.html", context)
+
+    def post(self, request):
+        user = User.objects.get(pk=request.user.id)
+        # profile = Profile()
+
+
+        user.first_name = request.POST.get("first_name")
+        user.last_name = request.POST.get("last_name")
+        user.email = request.POST.get("email")
+        user.profile.company = request.POST.get("company")
+        user.profile.bio = request.POST.get("bio")
+        user.profile.role = request.POST.get("role")
+
+
+        user.save()
+        return HttpResponseRedirect(reverse('clients:dashboard'))
+
+
+
