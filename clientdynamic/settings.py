c657c466e09d197b359b4ed4defd3aa974f3385a
vladyslavnUA <vladyslavn@yahoo.com>
2020-06-12 16:13:11 -0400

    client page

--- clientdynamic/settings.py
+++ clientdynamic/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'clientdynamic.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'clientdynamic.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
